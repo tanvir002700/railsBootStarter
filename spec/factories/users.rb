45f89b9252c9c2c62ba853076070896861c52579
tanvir002700 <tanvir002700@gmail.com>
2017-07-29 02:17:48 +0600

    update test code and flash message

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,5 @@
 FactoryGirl.define do
   factory :user do |u|
-    u.sequence(:first_name) { |n| "Test-#{n}" }
-    u.sequence(:last_name) { |n| "User-#{n}" }
     u.sequence(:email) { |n| "user#{n}@example.com" }
     password 'please123'
   end
