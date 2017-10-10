874edca5680bd4f53727d25664d4a23a4eb817a0
tanvir002700 <tanvir002700@gmail.com>
2017-10-10 23:29:49 +0600

    config route for api

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :home, only: [:index]
   root to: 'home#index'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      devise_for :users, controllers: {
+        sessions: 'api/v1/users/sessions'
+      }
+    end
+  end
 end
