78cd6a29bcfdcff423a5875cfbc69213e73d9f7e
tanvir002700 <tanvir002700@gmail.com>
2017-10-10 22:51:59 +0600

    add cors config in rake_cors file

--- config/application.rb
+++ config/application.rb
@@ -15,11 +15,5 @@ module RailsDemo
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
   end
 end
