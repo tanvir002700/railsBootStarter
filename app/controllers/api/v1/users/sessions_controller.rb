348cb5c01b658b568ba61132bc5114d8de019cc8
tanvir002700 <tanvir002700@gmail.com>
2017-10-11 00:28:48 +0600

    override device session_controller for sign_in

---
+++ app/controllers/api/v1/users/sessions_controller.rb
@@ -0,0 +1,19 @@
+module Api
+  module V1
+    class Users::SessionsController < Devise::SessionsController
+      require 'auth_token'
+      skip_before_action :verify_authenticity_token
+
+      def create
+        self.resource = warden.authenticate!(auth_options)
+        set_flash_message(:notice, :signed_in) if is_flashing_format?
+        sign_in(resource_name, resource)
+        yield resource if block_given?
+        token = AuthToken.issue_token({ user_id: resource.id })
+        respond_to do |format|
+          format.json { render json: {user: resource.email, token: token} }
+        end
+      end
+    end
+  end
+end
